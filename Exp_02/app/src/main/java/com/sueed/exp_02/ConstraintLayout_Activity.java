6613f8c12b616060e5c76f309d27eb05ea9dfdc7
Sueed <sueed003@gmail.com>
2020-10-06 00:26:42 +0800

    update linear and constraint

---
+++ Exp_02/app/src/main/java/com/sueed/exp_02/ConstraintLayout_Activity.java
@@ -0,0 +1,26 @@
+package com.sueed.exp_02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConstraintLayout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exp_02_constraint_layout);
+//        Button jump2 = findViewById(R.id.Three_One);
+//        jump2.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+}
---
+++ Exp_02/app/src/main/java/com/sueed/exp_02/TableLayout_Activity.java
@@ -0,0 +1,26 @@
+package com.sueed.exp_02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TableLayout_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exp_02_table_layout);
+//        Button jump3 = findViewById(R.id.Three_One);
+//        jump3.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent();
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+}
