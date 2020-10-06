058f34601ea2906e59d42703f5453dcd4696e871
Sueed <sueed003@gmail.com>
2020-10-06 13:33:10 +0800

    update for Exp_02

--- Exp_02/app/src/main/java/com/sueed/exp_02/ConstraintLayout_Activity.java
+++ Exp_02/app/src/main/java/com/sueed/exp_02/ConstraintLayout_Activity.java
@@ -13,14 +13,15 @@ public class ConstraintLayout_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exp_02_constraint_layout);
-//        Button jump2 = findViewById(R.id.Three_One);
-//        jump2.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Intent intent = new Intent();
-//                startActivity(intent);
-//            }
-//        });
+        Button jump2 = findViewById(R.id.Next);
+        jump2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setAction("jump_to_table");
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- Exp_02/app/src/main/java/com/sueed/exp_02/TableLayout_Activity.java
+++ Exp_02/app/src/main/java/com/sueed/exp_02/TableLayout_Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,14 +14,6 @@ public class TableLayout_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exp_02_table_layout);
-//        Button jump3 = findViewById(R.id.Three_One);
-//        jump3.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Intent intent = new Intent();
-//                startActivity(intent);
-//            }
-//        });
     }
 
 }
